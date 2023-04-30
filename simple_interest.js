be9eda24702ce619bbdab982a8a75be903261527
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-30 13:00:08 +0600

    simple interest calculator in javaScript

---
+++ simple_interest.js
@@ -0,0 +1,6 @@
+let balance = prompt("Enter Principal Balance: ");
+let int = prompt("Enter Annoul Interest: ");
+let time = prompt("Enter time in Year: ");
+let result = (balance * int * time) / 100;
+
+document.write("The Interest is: " + result);
